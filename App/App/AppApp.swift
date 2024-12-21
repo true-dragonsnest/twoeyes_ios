6426e74bd4e995b3cc5d78d571bef7bb7a8c1c93
phlox11 <ysk.phlox@gmail.com>
2024-12-21 21:53:56 +0900

    initial

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Yongsik Kim on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
